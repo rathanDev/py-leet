dba00996d9a688b9a23995a4c74198d907c2c99a
Jana <rathan.kparam@gmail.com>
2026-04-17 07:46:32 +0800

    x // 10 use integer division to remove last digit

---
+++ 00006_reverse_integer/local.py
@@ -0,0 +1,6 @@
+from solution import Solution
+
+s = Solution()
+x = 123
+rev: int = s.reverse(x)
+print(f"Number:{x} Rev:{rev}")
---
+++ 00006_reverse_integer/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+
+    def reverse(self, x: int) -> int:       # 123
+        neg: bool = x < 0
+        temp: int = abs(x)
+        rev: int = 0
+
+        while temp != 0:
+            digit: int = temp % 10      # 3   2     1
+            rev = (rev * 10) + digit    # 3   32    321
+            print(f"Rev:{rev}")
+            temp = temp // 10            # 12  1     0
+
+        if (neg):
+            return -rev
+        return rev
